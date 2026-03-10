fd69930022104041ea857fe8a0d849749abb09f1
Paul Schlosser <Schlosser.Paul@epa.gov>
2026-03-10 07:52:56 -0400

    Flouride.R script updated to read results from the Jean et al workbook and plot those vs. template version.

--- Flouride.R
+++ Flouride.R
@@ -9,26 +9,36 @@ setwd(script.dir)
 # Load functions to run the PBPK model template.
 source("run_template_model.R")
 
-times <- 0:251 # days
+times <- seq(0,91,by=0.25) # days
 out <- PBPK_run(model.param.filename = "F_template_parameters_Model.xlsx",
-                    model.param.sheetname = "Human", 
+                    model.param.sheetname = "Human4yo", 
                     exposure.param.filename = "F_template_parameters_Exposure.xlsx", 
                     exposure.param.sheetname = "Four_year_old", data.times=times*24)
 
- par(mfrow=c(1,2), mar=c(3,3,0,0), oma=c(1,0,1,1), mgp=c(1.5,0.5,0))
-  # Arterial blood
-  plot(times, out$C_art, type="l", xlab="Time (days)", xlim=c(0,251), xaxp=c(0,250,5), 
-       ylab="Arterial blood (mg/L)")
+Jean = cbind(read_excel("Data/Flouride/Model Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
+             range="A39:A86439"),
+             read_excel("Data/Flouride/Model Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
+                     range="D39:D86439"),
+             read_excel("Data/Flouride/Model Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
+                      range="AM39:AM86439"))
+colnames(Jean)=c("time","Ca","Cu")
 
+par(mfrow=c(1,2), mar=c(3,3,0,0), oma=c(1,0,1,1), mgp=c(1.5,0.5,0))
+  # Arterial blood
+  plot(times, out$C_art, type="l", xlab="Time (days)", xlim=c(0,91), xaxp=c(1,91,6), 
+       ylab="Arterial blood (mg/L)", ylim=c(0,0.015), yaxp=c(0,0.015,5))
+  lines(Jean$time,Jean$Ca, lty="dashed", lwd=3)
   
   # Urinary flouride
-  plot(d_200$time_blood_data, d_200$blood_data, pch=19, col=pub.col, 
-       log="y", xlab="Time (hr)", xlim=c(0,24), xaxp=c(0,24,6), 
-       ylab="Concentration (mg/L)", ylim=c(0.001, 1000))
-  lines(times, C_art[,2], col = templ.col5.1, lty = templ.lty, lwd = 3)
-  lines(times, C_fat[,2], col = templ.col5.2, lty = templ.lty, lwd = 3)
-  points(d_200$time_fat_data, d_200$fat_data, pch = 17, col = pub.col)
-  lines(d_200$time_blood_sim, d_200$blood_sim, col=pub.col5.1, lty=pub.lty, lwd=2)
-  lines(d_200$time_fat_sim, d_200$fat_sim, col=pub.col5.2, lty="dotdash", lwd=2)
-  title("200 ppm", line = -1.25)
+  plot(times, out$R_met_1st_om*24/(0.03*template$parms[["BW"]]), type="l", 
+       xlab="Time (days)", xlim=c(1,91), xaxp=c(1,91,6), 
+       ylab="Urinary flouride (mg/L)", ylim=c(0, 1.4), yaxp=c(0,1.4,7))
+  lines(Jean$time,Jean$Cu,, lty="dashed", lwd=3)
   
+  out2 <- PBPK_run(model.param.filename = "F_template_parameters_Model.xlsx",
+                  model.param.sheetname = "Human4yo", 
+                  exposure.param.filename = "F_template_parameters_Exposure.xlsx", 
+                  exposure.param.sheetname = "Four_year_old", data.times=times*24,
+                  adj.parms = c(T_iv_infuse=91*24))
+  plot(times, out2$C_art, type="l", xlab="Time (days)", xlim=c(0,91), xaxp=c(1,91,6), 
+       ylab="Arterial blood (mg/L)")
