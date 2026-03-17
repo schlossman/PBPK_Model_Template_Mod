cb2bfbf741b4a8e4b677526240fb657d9466081a
Paul Schlosser <Schlosser.Paul@epa.gov>
2026-03-17 16:33:23 -0400

    Final revisions to Flouride_Jean2018.R in this package: model creation is without verbose output, event timing/calculations revised to reduce errors from use of days vs hours, calculation of Y0["R_0Li"] corrected, integration tollerance increased, Jean (2018) results now read from a .csv file, plotting and second simulation lines revised to use previous changes and stand-alone .model code.

--- Flouride_Jean2018.R
+++ Flouride_Jean2018.R
@@ -10,18 +10,19 @@ library(MCSimMod) # Load MCSimMod
 script.dir = dirname(sys.frame(1)$ofile)
 setwd(script.dir)
 
-Flouride <- createModel(mName="Flouride", verboseOutput=TRUE)
+Flouride <- createModel(mName="Flouride", verboseOutput=FALSE) #TRUE)
 Flouride$loadModel()
 Flouride$updateParms()
 
 # Load functions to run the PBPK model template.
-days <- seq(0,91,by=0.25) # simulation days
+sim_days = 91
+times <- seq(0,sim_days,by=0.1)*24 # simulation days
 
 BW = 16; age = 4; ht = 103 # BW(kg), age(y), height(cm)
 Flouride$parms["BW0"] = BW
-Forc <- list(cbind(times=c(0,91), BW_in=c(BW,BW)),
-             cbind(times=c(0,91), age_in=c(age,age)),
-             cbind(times=c(0,91), height_in=c(ht,ht))
+Forc <- list(cbind(times=c(0,sim_days*24), BW_in=c(BW,BW)),
+             cbind(times=c(0,sim_days*24), age_in=c(age,age)),
+             cbind(times=c(0,sim_days*24), height_in=c(ht,ht))
 )
 
 # Exposures
@@ -38,36 +39,44 @@ diet_in = diet_ing*0.4*BW/24 # (mg/kg/d) x bio-availability x BW(kg) / (24 h/d) 
 toothpaste = paste_ing*1*BW/24 # (mg/kg/d) x bio-availability x BW(kg) / (24 h/d) = mg/h
 air_in = 1e-5*1*BW/24  # (mg/kg/d) x bio-availability x BW(kg) / (24 h/d) = mg/h
 
-Flouride$Y0["R_0Li"] = water_ing + soil_ing + diet_ing + paste_ing + air_in
-dose_end = data.frame(var=c("R_0Li"), time=exp_days*24, value=0, method="rep")
+Flouride$Y0["R_0Li"] = water_in + soil_in + diet_in + toothpaste + air_in
+dose_end = data.frame(var=c("R_0Li"), time=exp_days*24, value=0.0, method="rep")
 
-out <- Flouride$runModel(times=days*24, forcings = Forc, events=list(data=dose_end),
-                         rtol=1e-8, atol=1e-8, method="lsoda")
+out <- as.data.frame(Flouride$runModel(times=times, forcings=Forc, events=list(data=dose_end), 
+                                       rtol=1e-10, atol=1e-10, method="lsoda"))
 
-Jean = cbind(read_excel("Data/Flouride/Model Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
-             range="A39:A86439"),
-             read_excel("Data/Flouride/Model Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
-                     range="D39:D86439"),
-             read_excel("Data/Flouride/Model Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
-                      range="AM39:AM86439"))
-colnames(Jean)=c("time","Ca","Cu")
+#Jean = cbind(read_excel("Data/Flouride/Model_Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
+#             range="B38:B86439"),
+#             read_excel("Data/Flouride/Model_Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
+#                     range="D38:D86439"),
+#             read_excel("Data/Flouride/Model_Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
+#                      range="AM38:AM86439"),
+#             read_excel("Data/Flouride/Model_Fluoride_Child_90 daysSim_QA-copy_02-25-2026.xlsx", 
+#                        range="AE38:AE86439"))
+#colnames(Jean)=c("time","Ca","Cu","CL_BoNet")
+Jean=read.csv("Data/Flouride/Jean_2018_results.csv")
 
 par(mfrow=c(1,2), mar=c(3,3,0,0), oma=c(1,0,1,1), mgp=c(1.5,0.5,0))
   # Arterial blood
-  plot(times, out$C_art, type="l", xlab="Time (days)", xlim=c(0,91), xaxp=c(1,91,6), 
-       ylab="Arterial blood (mg/L)", ylim=c(0,0.015), yaxp=c(0,0.015,5))
-  lines(Jean$time,Jean$Ca, lty="dashed", lwd=3)
+  plot(out$time/24, out$C_Art, type="l", xlab="Time (days)", xlim=c(0,91), xaxp=c(1,91,6), 
+       ylab="Arterial blood (mg/L)", ylim=c(0,0.015), yaxp=c(0,0.015,5)) # 
+  lines(Jean$time/24,Jean$Ca, lty="dashed", lwd=3)
   
   # Urinary flouride
-  plot(times, out$R_met_1st_om*24/(0.03*template$parms[["BW"]]), type="l", 
+  plot(out$time/24, out$R_KiU*24/(0.03*Flouride$parms[["BW0"]]), type="l", 
        xlab="Time (days)", xlim=c(1,91), xaxp=c(1,91,6), 
        ylab="Urinary flouride (mg/L)", ylim=c(0, 1.4), yaxp=c(0,1.4,7))
-  lines(Jean$time,Jean$Cu,, lty="dashed", lwd=3)
+  lines(Jean$time/24,Jean$Cu,, lty="dashed", lwd=3)
   
-  out2 <- PBPK_run(model.param.filename = "F_template_parameters_Model.xlsx",
-                  model.param.sheetname = "Human4yo", 
-                  exposure.param.filename = "F_template_parameters_Exposure.xlsx", 
-                  exposure.param.sheetname = "Four_year_old", data.times=times*24,
-                  adj.parms = c(T_iv_infuse=91*24))
-  plot(times, out2$C_art, type="l", xlab="Time (days)", xlim=c(0,91), xaxp=c(1,91,6), 
-       ylab="Arterial blood (mg/L)")
+  # Bone "mineral" clearance rate
+  plot(out$time/24, out$R_BoMiNet, type="l", xlab="Time (days)", xlim=c(0,91), xaxp=c(1,91,6), 
+       ylab="Net CL to bone mineral (mg/h)")
+  lines(Jean$time/24,Jean$CL_BoNet, lty="dashed", lwd=3)
+  
+  # Simulation without stopping exposure
+  out2 <- as.data.frame(Flouride$runModel(times=times, forcings=Forc, rtol=1e-10, 
+                                          atol=1e-10, method="lsoda"))
+  plot(times/24, out2$C_Art, type="l", xlab="Time (days)", xlim=c(0,91), 
+       xaxp=c(1,91,6), ylab="Arterial blood with continuous dosing (mg/L)")
+  
+  
